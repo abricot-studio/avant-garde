0d3c5d4ca33afc9463108024e1d1c3433de3a045
Pacien Boisson <pakokrew@users.noreply.github.com>
2021-03-19 17:00:46 +0400

    Share pkg (#5)
    * Publish contracts
    
    * fix seed
    
    * test frontend lerna
    
    * revert using @packages
    
    * clean
    
    * Rm root tsconfig
    
    * revert tsconfig
    
    * Fix ts error

--- packages/subgraph/getAbi.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-const abi = require('@arbart/contracts/dist/ArbArt.abi.json');
-
-
-if (!fs.existsSync('./abi')) {
-  fs.mkdirSync('./abi');
-}
-fs.writeFileSync('./abi/ArbArt.json', JSON.stringify(abi));
