34c2f544b0f32b37f80f21e0017792f9da718541
julesGoullee <julesgoullee@gmail.com>
2021-03-21 12:49:51 +0400

    mint / burn with pricing

--- packages/contracts/contracts/ArbArt.sol
+++ packages/contracts/contracts/ArbArt.sol
@@ -7,11 +7,18 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract ArbArt is ERC721URIStorage, AccessControlEnumerable {
   using ECDSA for bytes32;
+  using Counters for Counters.Counter;
 
   bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
+  Counters.Counter countMint;
+
+  uint256 fees = 10; // 10%
+  uint256 baseFees = 100;
+  address payable fessReceiver;
 
   modifier onlyManager() { // Modifier
     require(
@@ -21,39 +28,91 @@ contract ArbArt is ERC721URIStorage, AccessControlEnumerable {
     _;
   }
 
-  constructor() ERC721("ArbArt", "ARBT") {
+  constructor(address payable _fessReceiver) ERC721("ArbArt", "ARBT") {
+    fessReceiver = _fessReceiver;
     _setupRole(MANAGER_ROLE, msg.sender);
+    countMint._value = 1;
   }
 
-  function _baseURI() internal override view returns (string memory) {
+  function _baseURI() internal override pure returns (string memory) {
     return "ipfs://";
   }
 
-  function mint(string memory _uri, address _signer, bytes memory _signature)
-  public
-  returns (uint256 _tokenId)
-  {
+  function mint(string memory _uri, address _signer, bytes memory _signature) public payable returns (uint256 _tokenId) {
+
     // Check signature and if signer is manager
     require(
       hasRole(MANAGER_ROLE, _signer),
       "Only accepting signatures from MANAGER_ROLE"
     );
     bytes memory _message = abi.encodePacked(_uri, msg.sender, _signer);
-    address _recoveredAddress = keccak256(_message)
-      .toEthSignedMessageHash()
-      .recover(_signature);
+    address _recoveredAddress = keccak256(_message).toEthSignedMessageHash().recover(_signature);
     require(_signer == _recoveredAddress, "Invalid recovered address");
+
+    // Check price
+    (uint256 price, uint256 mintFees) = currentMintPrice();
+    require(msg.value == price + mintFees, "ArbArt: amount invalid");
+    countMint.increment();
 
     // Mint token
     _tokenId = uint256(uint160(bytes20(msg.sender)));
-
     _safeMint(msg.sender, _tokenId);
     _setTokenURI(_tokenId, _uri);
+    sendValue(fessReceiver, mintFees);
 
     return _tokenId;
+
+  }
+
+  function burn(uint256 _tokenId) public returns (bool){
+
+    require(ownerOf(_tokenId) == msg.sender, "ArbArt: not owner");
+
+    countMint.decrement();
+    _burn(_tokenId);
+    sendValue(payable(msg.sender), currentPrice() );
+
+    return true;
+
+  }
+
+  // Price
+  function currentPrice() public view returns (uint256){
+    return priceFor(countMint.current() );
+  }
+
+  function currentMintPrice() public view returns (uint256 _currentPrice, uint256 _fees){
+    return mintPriceFor(countMint.current() );
+  }
+
+  function currentMintWithFeesPrice() public view returns (uint256){
+    return mintWithFeesPriceFor(countMint.current() );
+  }
+
+  function mintWithFeesPriceFor(uint256 _current) public view returns (uint256){
+    (uint256 mintPrice, uint256 mintFees) = mintPriceFor(_current);
+    return mintPrice + mintFees;
+  }
+
+  function mintPriceFor(uint256 _current) public view returns (uint256 _currentPrice, uint256 _fees){
+    _currentPrice = priceFor(_current);
+    _fees = _currentPrice * fees / baseFees;
+  }
+
+  function priceFor(uint256 _current) public view returns (uint256){
+    return _current ** 2 * (10 ** 18) / 10000; // x^2 / 10000
   }
 
   function supportsInterface(bytes4 interfaceId) public view override(ERC721, AccessControlEnumerable) returns (bool) {
     return AccessControlEnumerable.supportsInterface(interfaceId) || ERC721.supportsInterface(interfaceId);
   }
+
+  function sendValue(address payable recipient, uint256 amount) internal {
+    require(address(this).balance >= amount, "Address: insufficient balance");
+
+    // solhint-disable-next-line avoid-low-level-calls, avoid-call-value
+    (bool success, ) = recipient.call{ value: amount }("");
+    require(success, "Address: unable to send value, recipient may have reverted");
+  }
+
 }
--- packages/contracts/test/ArbArt.ts
+++ packages/contracts/test/ArbArt.ts
@@ -15,11 +15,14 @@ describe('ArbArt', function () {
   })
 
   beforeEach(async () => {
-    memory.contract = await memory.Contract.deploy()
-    await memory.contract.deployed()
 
     memory.manager = memory.signers[0]
+    memory.feesReceiver = memory.signers[memory.signers.length - 1]
     memory.other = memory.signers[1]
+    memory.other2 = memory.signers[2]
+    memory.contract = await memory.Contract.deploy(memory.feesReceiver.address)
+    await memory.contract.deployed()
+
   })
 
   it('sets up the contract', async () => {
@@ -31,21 +34,77 @@ describe('ArbArt', function () {
       .be.false
   })
 
+  it('priceFor', async () => {
+
+    // console.log('1', ethers.utils.formatEther(await memory.contract.priceFor('1') ));
+    // console.log('10', ethers.utils.formatEther(await memory.contract.priceFor('10') ));
+    // console.log('100', ethers.utils.formatEther(await memory.contract.priceFor('100') ));
+    // console.log('500', ethers.utils.formatEther(await memory.contract.priceFor('500') ));
+    // console.log('1000', ethers.utils.formatEther(await memory.contract.priceFor('1000') ));
+    // console.log('5000', ethers.utils.formatEther(await memory.contract.priceFor('5000') ));
+    // console.log('10000', ethers.utils.formatEther(await memory.contract.priceFor('10000') ));
+
+    expect(await memory.contract.priceFor('1') ).to.eq(ethers.utils.parseEther('0.0001') );
+    expect(await memory.contract.priceFor('10') ).to.eq(ethers.utils.parseEther('0.01') );
+    expect(await memory.contract.priceFor('100') ).to.eq(ethers.utils.parseEther('1') );
+    expect(await memory.contract.priceFor('500') ).to.eq(ethers.utils.parseEther('25') );
+    expect(await memory.contract.priceFor('1000') ).to.eq(ethers.utils.parseEther('100') );
+    expect(await memory.contract.priceFor('5000') ).to.eq(ethers.utils.parseEther('2500') );
+    expect(await memory.contract.priceFor('10000') ).to.eq(ethers.utils.parseEther('10000') );
+
+  });
+
+  it('mintWithFeesPriceFor', async () => {
+
+    // console.log('1', ethers.utils.formatEther(await memory.contract.mintWithFeesPriceFor('1') ));
+    // console.log('10', ethers.utils.formatEther(await memory.contract.mintWithFeesPriceFor('10') ));
+    // console.log('100', ethers.utils.formatEther(await memory.contract.mintWithFeesPriceFor('100') ));
+    // console.log('500', ethers.utils.formatEther(await memory.contract.mintWithFeesPriceFor('500') ));
+    // console.log('1000', ethers.utils.formatEther(await memory.contract.mintWithFeesPriceFor('1000') ));
+    // console.log('5000', ethers.utils.formatEther(await memory.contract.mintWithFeesPriceFor('5000') ));
+    // console.log('10000', ethers.utils.formatEther(await memory.contract.mintWithFeesPriceFor('10000') ));
+
+    expect(await memory.contract.mintWithFeesPriceFor('1') ).to.eq(ethers.utils.parseEther('0.00011') );
+    expect(await memory.contract.mintWithFeesPriceFor('10') ).to.eq(ethers.utils.parseEther('0.011') );
+    expect(await memory.contract.mintWithFeesPriceFor('100') ).to.eq(ethers.utils.parseEther('1.1') );
+    expect(await memory.contract.mintWithFeesPriceFor('500') ).to.eq(ethers.utils.parseEther('27.5') );
+    expect(await memory.contract.mintWithFeesPriceFor('1000') ).to.eq(ethers.utils.parseEther('110') );
+    expect(await memory.contract.mintWithFeesPriceFor('5000') ).to.eq(ethers.utils.parseEther('2750') );
+    expect(await memory.contract.mintWithFeesPriceFor('10000') ).to.eq(ethers.utils.parseEther('11000') );
+
+  });
+
   it('mint', async () => {
+
     const uri = 'Qmsfzefi221ifjzifj'
     const signature = await signMintingRequest(
       uri,
       memory.other.address,
       memory.manager
     )
+    const feesReceiverOriginalBalance = await ethers.provider.getBalance(memory.feesReceiver.address)
+    const minterOriginalBalance = await ethers.provider.getBalance(memory.other.address)
+    const mintWithFeesPrice = await memory.contract.currentMintWithFeesPrice()
+    expect(mintWithFeesPrice).to.eq(ethers.utils.parseEther('0.00011') )
 
-    await memory.contract
+    const txMint = await memory.contract
       .connect(memory.other)
-      .mint(uri, memory.manager.address, signature)
+      .mint(uri, memory.manager.address, signature, {
+        value: mintWithFeesPrice
+      })
+    const receiptMint = await txMint.wait();
+    const feesMintInEth = txMint.gasPrice.mul(receiptMint.gasUsed)
 
     const tokenId = memory.other.address
     expect(await memory.contract.ownerOf(tokenId)).to.eq(memory.other.address)
     expect(await memory.contract.tokenURI(tokenId)).to.eq(tokenURI(uri))
+    expect(await memory.contract.currentMintWithFeesPrice()).to.eq(ethers.utils.parseEther('0.00044') )
+    expect(await ethers.provider.getBalance(memory.contract.address) ).to.eq(ethers.utils.parseEther('0.0001') )
+    expect(await ethers.provider.getBalance(memory.feesReceiver.address) )
+      .to.eq(feesReceiverOriginalBalance.add(ethers.utils.parseEther('0.00001') ) )
+    expect(await ethers.provider.getBalance(memory.other.address) )
+      .to.eq(minterOriginalBalance.sub(feesMintInEth).sub(ethers.utils.parseEther('0.00011') ) )
+
   })
 
   it("can't mint two times", async () => {
@@ -55,14 +114,20 @@ describe('ArbArt', function () {
       memory.other.address,
       memory.manager
     )
+    const mintWithFeesPrice = await memory.contract.currentMintWithFeesPrice()
     await memory.contract
       .connect(memory.other)
-      .mint(uri, memory.manager.address, signature)
+      .mint(uri, memory.manager.address, signature, {
+        value: mintWithFeesPrice
+      })
 
+    const mintWithFeesPrice1 = await memory.contract.currentMintWithFeesPrice()
     await expect(
       memory.contract
         .connect(memory.other)
-        .mint(uri, memory.manager.address, signature)
+        .mint(uri, memory.manager.address, signature, {
+          value: mintWithFeesPrice1
+        })
     ).to.be.revertedWith('ERC721: token already minted')
   })
 
@@ -95,4 +160,133 @@ describe('ArbArt', function () {
         .mint(uri, memory.manager.address, invalidSignature)
     ).to.be.revertedWith('Invalid recovered address')
   })
+
+  it("can't mint with wrong value", async () => {
+    const uri = 'Qmsfzefi221ifjzifj'
+    const signature = await signMintingRequest(
+      uri,
+      memory.other.address,
+      memory.manager
+    )
+    await expect(
+      memory.contract
+        .connect(memory.other)
+        .mint(uri, memory.manager.address, signature, {
+          value: '123'
+        })
+    ).to.be.revertedWith('ArbArt: amount invalid')
+  })
+
+  it('mint and burn', async () => {
+
+    const uri = 'Qmsfzefi221ifjzifj'
+    const signature = await signMintingRequest(
+      uri,
+      memory.other.address,
+      memory.manager
+    )
+    const mintWithFeesPrice = await memory.contract.currentMintWithFeesPrice()
+    expect(mintWithFeesPrice).to.eq(ethers.utils.parseEther('0.00011') )
+
+    await memory.contract
+      .connect(memory.other)
+      .mint(uri, memory.manager.address, signature, {
+        value: mintWithFeesPrice
+      })
+
+    const tokenId = memory.other.address
+    const minterOriginalBalance = await ethers.provider.getBalance(memory.other.address)
+
+    const tx = await memory.contract
+      .connect(memory.other)
+      .burn(tokenId)
+
+    const receipt = await tx.wait();
+    const feesInEth = tx.gasPrice.mul(receipt.gasUsed)
+
+    await expect(memory.contract.tokenURI(tokenId)).to.be.revertedWith('ERC721URIStorage: URI query for nonexistent token')
+    expect(await ethers.provider.getBalance(memory.contract.address) ).to.eq(ethers.utils.parseEther('0') )
+    expect(await ethers.provider.getBalance(memory.other.address) )
+      .to.eq(minterOriginalBalance.sub(feesInEth).add(ethers.utils.parseEther('0.0001') ) )
+
+  })
+
+  it('mint and burn multiple', async () => {
+
+    const feesReceiverOriginalBalance = await ethers.provider.getBalance(memory.feesReceiver.address)
+
+    // mint w1
+    const uri1 = 'Qmsfzefi221ifjzifj'
+    const signature1 = await signMintingRequest(
+      uri1,
+      memory.other.address,
+      memory.manager
+    )
+    const mintWithFeesPrice1 = await memory.contract.currentMintWithFeesPrice()
+    expect(mintWithFeesPrice1).to.eq(ethers.utils.parseEther('0.00011') )
+    const tokenId1 = memory.other.address
+    await memory.contract
+      .connect(memory.other)
+      .mint(uri1, memory.manager.address, signature1, {
+        value: mintWithFeesPrice1
+      })
+
+
+    // mint w2
+    const uri2 = 'Qmsfzefi221ifjzifj2'
+    const signature2 = await signMintingRequest(
+      uri2,
+      memory.other2.address,
+      memory.manager
+    )
+    const mintWithFeesPrice2 = await memory.contract.currentMintWithFeesPrice()
+    expect(mintWithFeesPrice2).to.eq(ethers.utils.parseEther('0.00044') )
+    const tokenId2 = memory.other2.address
+    await memory.contract
+      .connect(memory.other2)
+      .mint(uri2, memory.manager.address, signature2, {
+        value: mintWithFeesPrice2
+      })
+
+    const mintWithFeesPriceEndMint = await memory.contract.currentMintWithFeesPrice()
+    expect(mintWithFeesPriceEndMint).to.eq(ethers.utils.parseEther('0.00099') )
+
+    // burn w1
+    const minterOriginal1Balance = await ethers.provider.getBalance(memory.other.address)
+
+    const txBurn1 = await memory.contract
+      .connect(memory.other)
+      .burn(tokenId1)
+
+    const receipt1 = await txBurn1.wait();
+    const feesInEth1 = txBurn1.gasPrice.mul(receipt1.gasUsed)
+
+    // burn w2
+
+    const minterOriginal2Balance = await ethers.provider.getBalance(memory.other2.address)
+
+    const txBurn2 = await memory.contract
+      .connect(memory.other2)
+      .burn(tokenId2)
+
+    const receipt2 = await txBurn2.wait();
+    const feesInEth2 = txBurn2.gasPrice.mul(receipt2.gasUsed)
+
+    expect(await ethers.provider.getBalance(memory.contract.address) ).to.eq(ethers.utils.parseEther('0') )
+    expect(await memory.contract.currentMintWithFeesPrice() ).to.eq(mintWithFeesPrice1)
+    expect(await ethers.provider.getBalance(memory.feesReceiver.address) )
+      .to.eq(feesReceiverOriginalBalance
+      .add(ethers.utils.parseEther('0.00004') )
+      .add(ethers.utils.parseEther('0.00001') ) )
+
+    // w1
+    expect(await ethers.provider.getBalance(memory.other.address) )
+      .to.eq(minterOriginal1Balance.sub(feesInEth1).add(ethers.utils.parseEther('0.0004') ) )
+
+    // w2
+    expect(await ethers.provider.getBalance(memory.other2.address) )
+      .to.eq(minterOriginal2Balance.sub(feesInEth2).add(ethers.utils.parseEther('0.0001') ) )
+
+  })
+
 })
