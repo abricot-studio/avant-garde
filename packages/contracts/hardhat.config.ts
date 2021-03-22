6e4f44d4db8c038dc20220f6c92049a40db5996d
julesGoullee <julesgoullee@gmail.com>
2021-03-22 18:57:11 +0400

    preprocessor remove console

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -5,14 +5,18 @@ import "@nomiclabs/hardhat-etherscan"
 import 'hardhat-deploy'
 import 'hardhat-deploy-ethers'
 import 'hardhat-gas-reporter'
+import { removeConsoleLog } from 'hardhat-preprocessor'
 import 'hardhat-tracer'
 import 'solidity-coverage'
 import { parseUnits } from 'ethers/lib/utils'
 
 const config: HardhatUserConfig = {
+  preprocess: {
+    eachLine: removeConsoleLog( (hre) => hre.network.name !== 'localhost'),
+  },
   gasReporter: {
     currency: 'EUR',
-    gasPrice: 21
+    gasPrice: parseUnits('5', 'gwei').toNumber()
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY
@@ -28,13 +32,13 @@ const config: HardhatUserConfig = {
     deployer: 0,
   },
   networks: {
-    local: {
+    localhost: {
       url: `http://localhost:8545`,
       accounts: ['0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'] //hardhat 0
     },
     ropsten: {
       url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
-      gasPrice: parseUnits('150', 'gwei').toNumber(),
+      gasPrice: parseUnits('5', 'gwei').toNumber(),
       accounts: [`0x${process.env.ROPSTEN_PRIVATE_KEY || 'ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'}`]
     }
   },
