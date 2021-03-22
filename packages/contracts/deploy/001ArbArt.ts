46ab330c63a19a99c270826c6a3347aa9512735d
julesGoullee <julesgoullee@gmail.com>
2021-03-22 18:44:03 +0400

    gasPrice && etherscan verification

--- packages/contracts/deploy/001ArbArt.ts
+++ packages/contracts/deploy/001ArbArt.ts
@@ -10,7 +10,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   await deploy('ArbArt', {
     from: a.deployer,
     args: [],
-    log: true,
+    log: true
   });
 };
 export default func;
--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -1,13 +1,22 @@
 import 'dotenv/config'
 import { HardhatUserConfig } from 'hardhat/types'
 import '@nomiclabs/hardhat-waffle'
+import "@nomiclabs/hardhat-etherscan"
 import 'hardhat-deploy'
 import 'hardhat-deploy-ethers'
-// import 'hardhat-gas-reporter'
+import 'hardhat-gas-reporter'
 import 'hardhat-tracer'
 import 'solidity-coverage'
+import { parseUnits } from 'ethers/lib/utils'
 
 const config: HardhatUserConfig = {
+  gasReporter: {
+    currency: 'EUR',
+    gasPrice: 21
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
   solidity: {
     compilers: [
       {
@@ -25,6 +34,7 @@ const config: HardhatUserConfig = {
     },
     ropsten: {
       url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      gasPrice: parseUnits('150', 'gwei').toNumber(),
       accounts: [`0x${process.env.ROPSTEN_PRIVATE_KEY || 'ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'}`]
     }
   },
