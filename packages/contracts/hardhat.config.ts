ed4faa85b476f859ae3033f3fa81cecf3e6423a6
Pacien Boisson <pakokrew@gmail.com>
2021-03-23 12:30:37 +0400

    account keys

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -10,6 +10,9 @@ import 'hardhat-tracer'
 import 'solidity-coverage'
 import { parseUnits } from 'ethers/lib/utils'
 
+const defaultPrivateKey = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80' // hardhat 0
+const defaultManagerAddress = '0xE4D29ec42F4057EfF92c9124c82844b2689f9C6d'
+
 const config: HardhatUserConfig = {
   preprocess: {
     eachLine: removeConsoleLog( (hre) => hre.network.name !== 'localhost' && hre.network.name !== 'hardhat'),
@@ -25,27 +28,31 @@ const config: HardhatUserConfig = {
     compilers: [
       {
         version: '0.8.1',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
       },
     ],
   },
   namedAccounts: {
     deployer: 0,
-    manager: 1,
+    manager: process.env.MANAGER_ADDRESS || defaultManagerAddress,
   },
   networks: {
     localhost: {
       url: `http://localhost:8545`,
       accounts: [
-        '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80', // hardhat 0
-        '0x630af0fbddb248b53f97ecf899ce11878d9dcd7e718574c92607153027632135' // 0xE4D29ec42F4057EfF92c9124c82844b2689f9C6d
+        defaultPrivateKey,
       ]
     },
     rinkeby: {
       url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
       gasPrice: parseUnits('5', 'gwei').toNumber(),
       accounts: [
-        `0x${process.env.RINKEBY_PRIVATE_KEY || 'ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'}`, // hardhat 0
-        '0x630af0fbddb248b53f97ecf899ce11878d9dcd7e718574c92607153027632135'// 0xE4D29ec42F4057EfF92c9124c82844b2689f9C6d
+        process.env.RINKEBY_PRIVATE_KEY || defaultPrivateKey,
       ]
     }
   },
