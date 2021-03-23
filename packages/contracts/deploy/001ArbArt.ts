100eef59db6ef4aabf7830e11ccd4f3a7a783825
Pacien Boisson <pakokrew@gmail.com>
2021-03-23 13:01:53 +0400

    Deploy script for subgraph and frontend

--- packages/contracts/deploy/001ArbArt.ts
+++ packages/contracts/deploy/001ArbArt.ts
@@ -1,17 +1,56 @@
+import * as fs from 'fs';
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {DeployFunction} from 'hardhat-deploy/types';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const contractName = 'ArbArt'
   const {deployments, getNamedAccounts} = hre;
   const {deploy} = deployments;
 
   const namedAccounts = await getNamedAccounts();
 
-  await deploy('ArbArt', {
+  const deployment = await deploy(contractName, {
     from: namedAccounts.deployer,
     args: [namedAccounts.manager],
     log: true
   });
+
+  const networkData = {
+    network: hre.network.name,
+    address: deployment.address,
+    startBlock: deployment.receipt?.blockNumber,
+  };
+
+  fs.writeFileSync(
+    `./deployments/${hre.network.name}/config.json`,
+    JSON.stringify(networkData, null, 2)
+  )
+  fs.writeFileSync(
+    `./deployments/${hre.network.name}/${contractName}.abi.json`,
+    JSON.stringify(deployment.abi, null, 2)
+  )
+
+  const chainId = hre.network.config.chainId
+  if(chainId) {
+
+    let networksABI: Record<number, any>
+    try {
+      networksABI = fs.readFileSync('./deployments/networks.json')
+    } catch(_) {
+      networksABI = {}
+    }
+
+    networksABI[chainId] = {
+      address: deployment.address,
+      abi: deployment.abi,
+    }
+
+    fs.writeFileSync(
+      './deployments/networks.json',
+      JSON.stringify(networksABI, null, 2)
+    )
+  }
+
 };
 export default func;
 func.tags = ['ArbArt'];
--- packages/frontend/lib/contracts.ts
+++ packages/frontend/lib/contracts.ts
@@ -1,9 +1,14 @@
-import abi from '../../contracts/dist/ArbArt.abi.json'
+import networks from '../../contracts/deployments/networks.json'
 import { Contract } from 'ethers'
 import { Provider } from "@ethersproject/abstract-provider";
 import { Signer } from "@ethersproject/abstract-signer";
 import config from '../config'
 
 export function getContract(providerOrSigner?: Signer | Provider) {
+  const chainId = 1; // TODO
+  const abi = networks[chainId]?.abi
+  if(!abi) {
+    throw new Error('unsupported network')
+  }
   return new Contract(config.contractAddress, abi, providerOrSigner)
 }
