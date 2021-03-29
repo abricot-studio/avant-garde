354cbd35e27e29fb95aeaeb5781119ecee7b95a0
julesGoullee <julesgoullee@gmail.com>
2021-03-29 19:01:35 +0400

    env

--- packages/frontend/config.ts
+++ packages/frontend/config.ts
@@ -3,10 +3,12 @@ export default {
     process.env.NEXT_PUBLIC_INFURA_ID || '781d8466252d47508e177b8637b1c2fd',
   ipfsEndpoint:
     process.env.NEXT_PUBLIC_IPFS_ENDPOINT ||
-    'https://gateway.pinata.cloud/ipfs/',
+    // 'https://gateway.pinata.cloud/ipfs/',
+    'https://cloudflare-ipfs.com/ipfs/',
   subgraphUrl:
     process.env.NEXT_PUBLIC_SUBGRAPH_URL ||
-    'http://127.0.0.1:8000/subgraphs/name/pakokrew/arbart',
+    'https://api.thegraph.com/subgraphs/name/julesgoullee/arbart',
+    // 'http://127.0.0.1:8000/subgraphs/name/pakokrew/arbart',
   generateUrl:
     process.env.NEXT_PUBLIC_GENERATE_URL ||
     'https://nftart-fct.vercel.app/api/generate',
