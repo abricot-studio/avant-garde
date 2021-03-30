132b76953dbb769d186e96608e34f13bce9a5b79
julesGoullee <julesgoullee@gmail.com>
2021-03-31 00:16:02 +0400

    TokenImage ScaleFade

--- packages/frontend/components/Home/Tokens.tsx
+++ packages/frontend/components/Home/Tokens.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import { Flex, Address, Wrap, WrapItem, Box, IconButton } from '../ui'
+import React, { useEffect } from 'react'
+import { Flex, Address, Wrap, WrapItem, Box, IconButton, ScaleFade } from '../ui'
 import { TokenImage } from '../ui/TokenImage'
 import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { useDisclosure } from '@chakra-ui/hooks'
 
 export function TokenCard({ token, size, showAddress }) {
   return (
@@ -38,10 +39,47 @@ export interface Props {
 export default function Tokens({ tokens, fetching, error }: Props) {
 
   const [index, setIndex] = React.useState(0)
+  const { isOpen, onToggle } = useDisclosure({
+    defaultIsOpen: true,
+  })
+
+  useEffect(() => {
+    !isOpen && setTimeout(onToggle, 200)
+  }, [isOpen])
 
   if (fetching) return <Box align="center" >Loading...</Box>
   if (error) return <Box align="center" >Oh no... {error.message}</Box>
-  const tokensDisplayed = tokens.slice(index, index + 3)
+
+  let tokensDisplayed
+
+  if(index + 3 > tokens.length){
+    tokensDisplayed = tokens
+      .slice(index, tokens.length)
+      .concat(tokens.slice(0, index + 3 - tokens.length) )
+  } else {
+    tokensDisplayed = tokens.slice(index, index + 3)
+  }
+
+  if(tokensDisplayed.length !== 3){
+
+    return (
+      <Flex
+        as="section"
+        direction="column"
+        align="center"
+      >
+        <Wrap spacing="30px" justify="center" align="center" m={8}>
+          {tokensDisplayed
+            .map( (token) => (
+              <WrapItem key={token.id}>
+                <TokenCard size={250} showAddress={true} token={token}/>
+              </WrapItem>
+            ))}
+        </Wrap>
+      </Flex>
+    )
+
+  }
 
   return (
     <Flex
@@ -50,22 +88,26 @@ export default function Tokens({ tokens, fetching, error }: Props) {
       align="center"
     >
       <Wrap spacing="30px" justify="center" align="center" m={8}>
-        <WrapItem key={tokensDisplayed[0].id}>
-          <TokenCard size={250} showAddress={false} token={tokensDisplayed[0]}/>
-        </WrapItem>
+          <WrapItem key={tokensDisplayed[0].id}>
+            <ScaleFade initialScale={0} in={isOpen}>
+              <TokenCard size={250} showAddress={false} token={tokensDisplayed[0]}/>
+            </ScaleFade>
+          </WrapItem>
         <IconButton
           icon={<FontAwesomeIcon icon={faArrowLeft} size="1x" />}
           aria-label="Back"
           colorScheme="transparent"
           color="grey"
           _hover={{}}
-          onClick={() => setIndex(index +1 )}
+          onClick={() => setIndex(index === 0 ? tokens.length -1 : index - 1)}
           _focus={{
             outline: "none"
           }}
         />
         <WrapItem key={tokensDisplayed[1].id}>
-          <TokenCard size={350} showAddress={true} token={tokensDisplayed[1]}/>
+          <ScaleFade initialScale={0} in={isOpen} >
+            <TokenCard size={350} showAddress={true} token={tokensDisplayed[1]}/>
+          </ScaleFade>
         </WrapItem>
         <IconButton
           icon={<FontAwesomeIcon icon={faArrowRight} size="1x" />}
@@ -73,13 +115,18 @@ export default function Tokens({ tokens, fetching, error }: Props) {
           colorScheme="transparent"
           color="grey"
           _hover={{}}
-          onClick={() => setIndex(index +1 )}
+          onClick={() => {
+            onToggle()
+            setIndex(index === tokens.length -1 ? 0 : index + 1)
+          }}
           _focus={{
             outline: "none"
           }}
         />
         <WrapItem key={tokensDisplayed[2].id}>
-          <TokenCard size={250} showAddress={false} token={tokensDisplayed[2]}/>
+          <ScaleFade initialScale={0} in={isOpen} >
+            <TokenCard size={250} showAddress={false} token={tokensDisplayed[2]}/>
+          </ScaleFade>
         </WrapItem>
       </Wrap>
     </Flex>
