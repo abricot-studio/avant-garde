ad310d9efc7cf003fbbe2f848717f93a3f7fac70
Pacien Boisson <pakokrew@gmail.com>
2021-04-07 20:08:26 +0400

    Network checker

--- packages/frontend/components/utils/NetworkChecker.tsx
+++ packages/frontend/components/utils/NetworkChecker.tsx
@@ -1,35 +1,40 @@
 import { useMemo } from 'react'
-import { Modal, ModalOverlay, ModalHeader, ModalBody, ModalContent, Text } from '../ui'
+import { getChainName } from '@usedapp/core'
+import { Modal, ModalOverlay, ModalHeader, ModalBody, ModalContent, Text, Button } from '../ui'
 import { useWeb3 } from '../../contexts/Web3Context'
 import networks from '../../../contracts/deployments/networks.json'
 import config from '../../config'
 
-const networkName = {
-  1: 'Mainnet',
-  4: 'Rinkeby',
-}
-
 export function NetworkChecker({ children }) {
-  const {account} = useWeb3()
+  const {account, disconnect} = useWeb3()
 
   const contractInfo = useMemo(() => account?.chainId && networks[account.chainId], [account]);
 
   if(account && !contractInfo) {
     return (
-        <Modal isOpen isCentered onClose={() => 0}>
-          <ModalOverlay />
-          <ModalContent pt={2} pb={4}>
-            <ModalHeader textAlign="center">Unsupported network</ModalHeader>
-            <ModalBody textAlign="center">
-              <Text>
-                Please switch your wallet to a supported network:
-              </Text>
-              <Text textStyle="caption" colorScheme="red">
-                {networkName[config.defaultChainId] || config.defaultChainId}
-              </Text>
-            </ModalBody>
-          </ModalContent>
-        </Modal>
+      <Modal isOpen isCentered onClose={() => 0}>
+        <ModalOverlay />
+        <ModalContent pt={2} pb={4}>
+          <ModalHeader textAlign="center">Unsupported network</ModalHeader>
+          <ModalBody textAlign="center">
+            <Text>
+              Please switch your wallet to a supported network:
+            </Text>
+            <Text textStyle="caption" colorScheme="red">
+              {getChainName(config.defaultChainId) || config.defaultChainId}
+            </Text>
+            <Button
+              onClick={disconnect}
+              variant="outline"
+              colorScheme="red"
+              mt={4}
+            >
+              Disconnect
+            </Button>
+
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     )
   }
 
