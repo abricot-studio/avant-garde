52ee5cdfa4790a47195d7b95a67fc22f7ac3329c
julesGoullee <julesgoullee@gmail.com>
2021-05-04 17:29:57 +0400

    add kovan network

--- packages/frontend/lib/web3.ts
+++ packages/frontend/lib/web3.ts
@@ -3,11 +3,12 @@ import { Config, ChainId } from '@usedapp/core';
 import config from '../config'
 
 export const DAppConfig: Config = {
-  supportedChains: [ChainId.Mainnet, ChainId.Rinkeby, ChainId.Localhost],
+  supportedChains: [ChainId.Mainnet, ChainId.Rinkeby, ChainId.Kovan, ChainId.Localhost],
   readOnlyChainId: config.defaultChainId,
   readOnlyUrls: {
     [ChainId.Mainnet]: `https://mainnet.infura.io/v3/${config.infuraId}`,
     [ChainId.Rinkeby]: `https://rinkeby.infura.io/v3/${config.infuraId}`,
+    [ChainId.Kovan]: `https://kovan.infura.io/v3/${config.infuraId}`,
     [ChainId.Localhost]: `http://localhost:8545`,
   },
 }
