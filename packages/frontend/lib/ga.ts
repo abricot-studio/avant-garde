d76466814dc0e864276d062d07139ce3a29a324a
julesGoullee <julesgoullee@gmail.com>
2021-06-13 16:50:03 +0400

    log

--- packages/frontend/lib/ga.ts
+++ packages/frontend/lib/ga.ts
@@ -1,7 +1,7 @@
 import config from '../config'
 
 export const event = ({ action, params }) => {
-  console.log('ga event', { action, params })
+  // console.log('ga event', { action, params })
   if(config.enableAnalytics){
     // @ts-ignore
     window.gtag('event', action, params)
