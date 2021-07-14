017532da76583aaf94b9d99fb99b0e0ea8bfc4a6
julesGoullee <julesgoullee@gmail.com>
2021-07-14 14:27:15 +0400

    gas

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -71,7 +71,7 @@ const config: HardhatUserConfig = {
     },
     mainnet: {
       url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
-      gasPrice: parseUnits('12', 'gwei').toNumber(),
+      gasPrice: parseUnits('19', 'gwei').toNumber(),
       accounts: [process.env.MAINNET_PRIVATE_KEY || defaultPrivateKey],
     },
   },
