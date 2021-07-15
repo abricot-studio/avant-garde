2936d422b7fa53dfbff5d6c21b36dc4bac0bbe23
julesGoullee <julesgoullee@gmail.com>
2021-07-16 00:13:36 +0400

    bannerInvite

--- packages/frontend/pages/generator.tsx
+++ packages/frontend/pages/generator.tsx
@@ -3,9 +3,11 @@ import Layout from '../components/Layout'
 import Generate from '../components/pages/Generate'
 import { Heading } from '../components/ui'
 import SEO from '../components/utils/SEO'
+const domain = process.env.APP_URL || 'https://avant-garde.gallery'
 
 const seoData = {
   title: 'Generator',
+  card: `${domain}/bannerInvite.png`,
 }
 
 const Generator: React.FC = () => {
