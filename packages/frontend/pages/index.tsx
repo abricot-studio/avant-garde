c63aac8f0513d0668030d0e9cd961b5f140d4509
julesGoullee <julesgoullee@gmail.com>
2021-09-01 19:35:55 +0200

    history style

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import Hero from '../components/pages/Hero'
-import { Box, Heading, Wrap, WrapItem } from '../components/ui'
+import { Box, Flex, Heading, Wrap, WrapItem } from '../components/ui'
 import SEO from '../components/utils/SEO'
 import config from '../config'
 import { useHistory } from '../hooks/history'
@@ -20,30 +20,34 @@ const Home: React.FC = () => {
           Home
         </Heading>
         <Hero />
-        <Wrap spacing={16} justify="center" mx={24}>
-          <WrapItem flexDirection="column" alignItems="center">
-            <Box fontWeight={700} fontSize={24}>
-              {minted}
-            </Box>
-            <Box>Minted</Box>
+        <Wrap spacing={4} justify="center" mx={12}>
+          <WrapItem alignItems="center">
+            <Flex flexDirection="column" alignItems="center" mx={4}>
+              <Box fontWeight={700} fontSize={24}>
+                {minted}
+              </Box>
+              <Box>Minted</Box>
+            </Flex>
+            <Flex flexDirection="column" alignItems="center" mx={4}>
+              <Box fontWeight={700} fontSize={24}>
+                Ξ {balancePool}
+              </Box>
+              <Box>Pool</Box>
+            </Flex>
           </WrapItem>
-          <WrapItem flexDirection="column" alignItems="center">
-            <Box fontWeight={700} fontSize={24}>
-              Ξ {balancePool}
-            </Box>
-            <Box>Pool</Box>
-          </WrapItem>
-          <WrapItem flexDirection="column" alignItems="center">
-            <Box fontWeight={700} fontSize={24}>
-              {holders}
-            </Box>
-            <Box>Holders</Box>
-          </WrapItem>
-          <WrapItem flexDirection="column" alignItems="center">
-            <Box fontWeight={700} fontSize={24}>
-              {burned}
-            </Box>
-            <Box>Burned</Box>
+          <WrapItem  alignItems="center">
+            <Flex flexDirection="column" alignItems="center" mx={4}>
+              <Box fontWeight={700} fontSize={24}>
+                {holders}
+              </Box>
+              <Box>Holders</Box>
+            </Flex>
+            <Flex flexDirection="column" alignItems="center" mx={4}>
+              <Box fontWeight={700} fontSize={24}>
+                {burned}
+              </Box>
+              <Box>Burned</Box>
+            </Flex>
           </WrapItem>
         </Wrap>
       </Layout>
