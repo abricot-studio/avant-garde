4e3eaf53fad5a27bbfb793fff8d0bf8f7fc7211a
julesGoullee <julesgoullee@gmail.com>
2021-09-04 17:37:01 +0200

    fix

--- packages/frontend/components/tokens/Token.tsx
+++ packages/frontend/components/tokens/Token.tsx
@@ -167,7 +167,7 @@ ${permalink}
 
 ${tags}`
       const canvas = canvasRef.current
-      if (canvas) {
+      if (canvas && metadata && metadata.image) {
         const context = canvas.getContext('2d')
         context.clearRect(0, 0, 512, 512)
         const image = new Image()
